0d2716845fd976557c8b113e1351d494be8a3e03
marcossalves <marcossa.ti@terra.com.br>
2020-07-20 18:31:11 -0300

    estrutura modelo curríulo

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -1,13 +1,16 @@
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
+import { Ionicons, FontAwesome5 } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+import MarcosScreen from "../screens/MarcosScreen";
+import FormacaoScreen from "../screens/FormacaoScreen";
+import ObjetivoScreen from "../screens/ObjetivoScreen";
+import ContatoScreen from "../screens/ContatoScreen";
+
+import { BottomTabParamList, TabOneParamList, TabTwoParamList } from "../types";
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
@@ -17,19 +20,41 @@ export default function BottomTabNavigator() {
   return (
     <BottomTab.Navigator
       initialRouteName="TabOne"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
+      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
+    >
       <BottomTab.Screen
-        name="TabOne"
-        component={TabOneNavigator}
+        name="Marcos"
+        component={MarcosNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="male" color={color} />,
         }}
       />
+
       <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoNavigator}
+        name="Formacao"
+        component={FormacaoNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="graduation-cap" color={color} />
+          ),
+        }}
+      />
+
+      <BottomTab.Screen
+        name="Objetivo"
+        component={ObjetivoNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="eye" color={color} />,
+        }}
+      />
+
+      <BottomTab.Screen
+        name="Contato"
+        component={ContatoNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="phone-square" color={color} />
+          ),
         }}
       />
     </BottomTab.Navigator>
@@ -39,35 +64,63 @@ export default function BottomTabNavigator() {
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
 function TabBarIcon(props: { name: string; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+  return <FontAwesome5 size={30} style={{ marginBottom: -3 }} {...props} />;
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
 // https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
-const TabOneStack = createStackNavigator<TabOneParamList>();
 
-function TabOneNavigator() {
+const MarcosStack = createStackNavigator<TabTwoParamList>();
+
+function MarcosNavigator() {
   return (
-    <TabOneStack.Navigator>
-      <TabOneStack.Screen
-        name="TabOneScreen"
-        component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
+    <MarcosStack.Navigator>
+      <MarcosStack.Screen
+        name="MarcosScreen"
+        component={MarcosScreen}
+        options={{ headerTitle: "Marcos" }}
       />
-    </TabOneStack.Navigator>
+    </MarcosStack.Navigator>
   );
 }
 
-const TabTwoStack = createStackNavigator<TabTwoParamList>();
+const FormacaoStack = createStackNavigator<TabTwoParamList>();
 
-function TabTwoNavigator() {
+function FormacaoNavigator() {
   return (
-    <TabTwoStack.Navigator>
-      <TabTwoStack.Screen
-        name="TabTwoScreen"
-        component={TabTwoScreen}
-        options={{ headerTitle: 'Tab Two Title' }}
+    <FormacaoStack.Navigator>
+      <FormacaoStack.Screen
+        name="FormacaoScreen"
+        component={FormacaoScreen}
+        options={{ headerTitle: "Formacao" }}
       />
-    </TabTwoStack.Navigator>
+    </FormacaoStack.Navigator>
+  );
+}
+
+const ObjetivoStack = createStackNavigator<TabTwoParamList>();
+
+function ObjetivoNavigator() {
+  return (
+    <ObjetivoStack.Navigator>
+      <ObjetivoStack.Screen
+        name="ObjetivoScreen"
+        component={ObjetivoScreen}
+        options={{ headerTitle: "Objetivo" }}
+      />
+    </ObjetivoStack.Navigator>
+  );
+}
+
+const ContatoStack = createStackNavigator<TabTwoParamList>();
+function ContatoNavigator() {
+  return (
+    <ContatoStack.Navigator>
+      <ContatoStack.Screen
+        name="ContatoScreen"
+        component={ContatoScreen}
+        options={{ headerTitle: "Contato" }}
+      />
+    </ContatoStack.Navigator>
   );
 }
